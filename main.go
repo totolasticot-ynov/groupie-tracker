823b58833e2c19faefafe05580829ec2dd7f296c
totolasticot-ynov <thomas.dhordain@ynov.com>
2025-12-08 10:32:53 +0100

    envoie sur pc

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 type Artist struct {
 	Id    int    `json:"id"`
 	Name  string `json:"name"`
-	Image string `json:"image"` // Servira ici pour la pochette de l'album
+	Image string `json:"image"`
 }
 
 // Structure Relations
@@ -26,7 +26,7 @@ type PageData struct {
 }
 
 func main() {
-	// Important : On gère le sous-dossier img pour les images
+	// C'est cette ligne qui permet d'afficher tes images locales
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
@@ -37,21 +37,20 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	// 1. Récupérer les artistes de l'API
+	// 1. Récupérer les artistes de l'API officielle
 	apiArtists, err := getArtists()
 	if err != nil {
-		log.Println("Erreur API:", err)
-		// On continue même si l'API plante, pour afficher tes artistes perso
+		log.Println("Attention : Impossible de contacter l'API (Internet coupé ?)")
+		// On continue quand même pour afficher tes artistes perso
 	}
 
-	// 2. Créer tes artistes personnalisés
+	// 2. Récupérer tes artistes personnalisés (Images locales)
 	myArtists := getCustomArtists()
 
-	// 3. Fusionner les deux listes (Les tiens en premier !)
-	// On ajoute 'apiArtists' à la suite de 'myArtists'
+	// 3. Fusionner les deux listes (Tes artistes en premier)
 	allArtists := append(myArtists, apiArtists...)
 
-	// Pour l'exemple des relations (inchangé)
+	// Récupérer une relation pour l'exemple (ID 1)
 	relation, _ := getRelation(1)
 
 	data := PageData{
@@ -61,27 +60,27 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	tmpl, err := template.ParseFiles("templates/index.html")
 	if err != nil {
-		http.Error(w, err.Error(), 500)
+		http.Error(w, "Erreur : Impossible de lire index.html. Vérifie le dossier templates.", 500)
 		return
 	}
 
 	tmpl.Execute(w, data)
 }
 
-// Ta liste personnalisée avec les pochettes d'albums
+// C'est ici qu'on fait le lien avec tes fichiers dans le dossier "img"
 func getCustomArtists() []Artist {
 	return []Artist{
-		{Id: 101, Name: "Aphex Twin", Image: "https://upload.wikimedia.org/wikipedia/en/8/82/Selected_Ambient_Works_85-92.png"},                                      // Selected Ambient Works
-		{Id: 102, Name: "Crystal Castles", Image: "https://upload.wikimedia.org/wikipedia/en/a/ae/Crystal_Castles_%28album%29.png"},                                  // Crystal Castles (I)
-		{Id: 103, Name: "Ennio Morricone", Image: "https://upload.wikimedia.org/wikipedia/en/0/03/The_Good_the_Bad_and_the_Ugly_soundtrack_cover.jpg"},               // The Good, The Bad...
-		{Id: 104, Name: "Rihanna", Image: "https://upload.wikimedia.org/wikipedia/en/3/32/Rihanna_-_Anti.png"},                                                       // Anti
-		{Id: 105, Name: "Daft Punk", Image: "https://upload.wikimedia.org/wikipedia/en/a/ae/Daft_Punk_-_Discovery.jpg"},                                              // Discovery
-		{Id: 106, Name: "TV Girl", Image: "https://upload.wikimedia.org/wikipedia/en/3/30/TV_Girl_-_French_Exit.png"},                                                // French Exit
-		{Id: 107, Name: "Björk", Image: "https://upload.wikimedia.org/wikipedia/en/a/a6/Bjork_Homogenic.png"},                                                        // Homogenic
-		{Id: 108, Name: "Nirvana", Image: "https://upload.wikimedia.org/wikipedia/en/b/b7/NirvanaNevermindalbumcover.jpg"},                                           // Nevermind
-		{Id: 109, Name: "Snow Strippers", Image: "https://e.snmc.io/i/600/s/5712f55977a4505030245a4911005f77/10839843/snow-strippers-april-mixtape-3-Cover-Art.jpg"}, // April Mixtape 3 (Exemple)
-		{Id: 110, Name: "Venetian Snares", Image: "https://upload.wikimedia.org/wikipedia/en/2/22/Rossz_csillag_alatt_született.jpg"},                                // Rossz Csillag...
-		{Id: 111, Name: "Boards of Canada", Image: "https://upload.wikimedia.org/wikipedia/en/e/e6/Boards_of_Canada_-_Music_Has_the_Right_to_Children.png"},          // Music Has the Right to Children
+		{Id: 101, Name: "Aphex Twin", Image: "/static/img/aphex.jpg"},
+		{Id: 102, Name: "Crystal Castles", Image: "/static/img/crystal.jpg"},
+		{Id: 103, Name: "Ennio Morricone", Image: "/static/img/ennio.jpg"},
+		{Id: 104, Name: "Rihanna", Image: "/static/img/rihanna.jpg"},
+		{Id: 105, Name: "Daft Punk", Image: "/static/img/daftpunk.jpg"},
+		{Id: 106, Name: "TV Girl", Image: "/static/img/tvgirl.jpg"},
+		{Id: 107, Name: "Björk", Image: "/static/img/bjork.jpg"},
+		{Id: 108, Name: "Deftones", Image: "/static/img/deftones.jpg"},
+		{Id: 109, Name: "Snow Strippers", Image: "/static/img/snow.jpg"},
+		{Id: 110, Name: "Venetian Snares", Image: "/static/img/venetian.jpg"},
+		{Id: 111, Name: "Boards of Canada", Image: "/static/img/boc.jpg"},
 	}
 }
 
@@ -96,7 +95,7 @@ func getArtists() ([]Artist, error) {
 	return artists, nil
 }
 
-func getRelation(id int) (Relations, error) {
+func getRelation(int) (Relations, error) {
 	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/relations/1")
 	if err != nil {
 		return Relations{}, err
